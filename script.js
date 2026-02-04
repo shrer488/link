73590abba4f3270f9f35c9b7dfc8c398a79d596d
Riya Shrestha <shrer488@newschool.edu>
2026-02-04 09:33:16 -0500

    Added individual variables for each of the collection, fixed height issue of pop up.
    New issue: dont want the back of the pop up to be scrollable.

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-let modalButton = document.querySelector('.modal')
-let modalDialog = document.querySelector('.dialog')
-let closeButton = modalDialog.querySelector('button')
+// Modal Buttons
+let imageModalButton = document.querySelector('.image-modal')
+let videoModalButton = document.querySelector ('.video-modal')
+let linkModalButton = document.querySelector ('.link-modal')
 
-modalButton.addEventListener('click',()=>{
-    modalDialog.showModal()
+// Dialog/popup boxes
+let imageDialog = document.querySelector('.image-dialog')
+let videoDialog = document.querySelector('.video-dialog')
+let linkDialog = document.querySelector('.link-dialog')
+
+// Close buttons
+let imageClose = imageDialog.querySelector('button')
+let videoClose = videoDialog.querySelector('button')
+let linkClose = linkDialog.querySelector('button')
+
+// -----------------------------------------------------------------
+
+// Image
+imageModalButton.addEventListener('click',()=>{
+    imageDialog.showModal()
+})
+imageClose.addEventListener('click', ()=>{
+    imageDialog.close()
 })
 
-closeButton.addEventListener('click', ()=>{
-    modalDialog.close()
+// Video 
+videoModalButton.addEventListener('click',()=>{
+    videoDialog.showModal()
+})
+videoClose.addEventListener('click', ()=>{
+    videoDialog.close()
+})
+
+// Link
+linkModalButton.addEventListener('click',()=>{
+    linkDialog.showModal()
+})
+linkClose.addEventListener('click', ()=>{
+    linkDialog.close()
 })
